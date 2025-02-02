7a96f2c15434351d67ef9670625a5fa102e0d32b
Lukasz <lukasz.milos.dev@gmail.com>
2025-02-02 14:43:04 +0100

    fix errors

--- src/content-config.ts
+++ src/content-config.ts
@@ -1,5 +1,5 @@
-import { defineCollection } from "astro:content"
-import { glob, file } from 'astro/loaders';
+// import { defineCollection } from "astro:content"
+// import { glob, file } from 'astro/loaders';
 
 // const blog = defineCollection({
 //   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/blog" }),
